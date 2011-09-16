f1b7a7c53d875de533c509e682890b2cccfd7327
Jean-Marie <jean-marie.ferdegue@newsint.co.uk>
2011-09-16 16:26:10 +0100

    missing features : - ability to build a link from the UI - graphical enhancements - lockdown to users (requires Google team involvement)

--- buildandconfigteam/nimobileapps/src/main.py
+++ buildandconfigteam/nimobileapps/src/main.py
@@ -15,10 +15,14 @@
 # limitations under the License.
 #
 from google.appengine.ext import webapp
+from django.utils import simplejson
 from google.appengine.ext.webapp import util
 from google.appengine.ext import db
 from google.appengine.ext.webapp import template
-
+from google.appengine.api import users
+from google.appengine.ext import blobstore
+from google.appengine.ext.webapp import blobstore_handlers  
+from google.appengine.api.images import get_serving_url
 from time import *
 
 import hashlib
@@ -29,37 +33,133 @@ import os
 PRIVATEKEY="48jfgfs"
 BACKEND="http://vm-sles11-239/s3fileserver/"
 
+###################################################################################
+###################################################################################
+###### DATASTORE DECLARATION
+###################################################################################
+###################################################################################
+class auditTrail(db.Model):
+    user = db.UserProperty(auto_current_user=True)
+    description = db.StringProperty(multiline=False)
+    logDate = db.DateTimeProperty(auto_now_add=True)
+
+class project(db.Model):
+    picture = db.StringProperty(multiline=False)
+    logDate = db.DateTimeProperty(auto_now_add=True)
+    user = db.UserProperty(auto_current_user=True)
+    active = db.BooleanProperty(default=True)
+    
+class projectStage(db.Model):
+    project = db.ReferenceProperty(project, required=True, collection_name='project')
+    name = db.StringProperty(multiline=False)
+    logDate = db.DateTimeProperty(auto_now_add=True)
+    user = db.UserProperty(auto_current_user=True)
+
+class awsLink(db.Model):
+    user = db.UserProperty(auto_current_user=True)
+    logDate = db.DateTimeProperty(auto_now_add=True)
+    projectStage = db.ReferenceProperty(projectStage, required=True, collection_name='projectStage')
+    generatedLink = db.StringProperty(multiline=False)
+    
+    def get(self,projectStageKey):
+        self.response.out.write("great")
+        
+###################################################################################
+###################################################################################
+###### END OF DATASTORE DECLARATION
+###################################################################################
+###################################################################################
+
+
 class MainHandler(webapp.RequestHandler):
     def get(self):
-        self.response.out.write('Hello world!')
+        projectsDb = db.Query(project).filter("active =",True)
+        projects = []
+        for oneProject in projectsDb:
+            projectsStageDb = db.Query(projectStage).filter("project =",oneProject)
+            stages=[]
+            for oneProjectStage in projectsStageDb:
+                stages.append({'name':oneProjectStage.name})
+            newP={
+                  'name':oneProject.key().name(),
+                  'picture':oneProject.picture,
+                  'stages':stages,
+                  }
+            projects.append(newP)
+        template_values = {
+            'projects': projects,
+        }
+        path = os.path.join(os.path.dirname(__file__), 'template','MainHandler.htm')
+        self.response.out.write(template.render(path, template_values))
+        
+class addProject(blobstore_handlers.BlobstoreUploadHandler):
+    def get(self):
+        template_values = {
+            'uploadLink': blobstore.create_upload_url('/addProject'),
+        }
+        if self.request.get('message'):
+            template_values['message']=self.request.get('message')
+        path = os.path.join(os.path.dirname(__file__), 'template','addProject.htm')
+        self.response.out.write(template.render(path, template_values))
+        
+    def post(self):
+        newProject = project.get_or_insert(key_name=self.request.get('name'))
+        
+        upload_files = self.get_uploads('picture')  # 'file' is file upload field in the form
+        blob_info = upload_files[0]
+        newProject.picture=get_serving_url(blob_info.key())
+        newProject.save()
+        self.redirect("/addProject?%s" % urllib.urlencode({"message": str(newProject.key().name())+" successfully added"}))
+
+class addProjectStage(webapp.RequestHandler):
+    def get(self):
+        projectsDb = db.Query(project).filter("active =",True)
+        projects = []
+        for oneProject in projectsDb:
+            oneProject.name = oneProject.key().name()
+            projects.append(oneProject)
+        template_values = {
+            'projects': projects,
+        }
+        if self.request.get('message'):
+            template_values['message']=self.request.get('message')
+        path = os.path.join(os.path.dirname(__file__), 'template','addProjectStage.htm')
+        self.response.out.write(template.render(path, template_values))
+        
+    def post(self):
+        newProjectStage = projectStage.get_or_insert(key_name=self.request.get('project')+"_"+self.request.get('name'),project=project.get_by_key_name(self.request.get('project')), name=self.request.get('name')) 
+        self.redirect("/addProjectStage?%s" % urllib.urlencode({"message": str(newProjectStage.name)+" successfully added to project " + str(newProjectStage.project.key().name())}))
+
 
 class buildPlist(webapp.RequestHandler):
     def get(self,project,environment,artifact):
-		validFrom=str(int(time()))
-		artifactUnencoded=artifact.replace("|",'/').replace("%7C",'/')
-		strToSign=PRIVATEKEY+"+artifact="+artifactUnencoded+"+environment="+environment+"+project="+project+"+validFrom="+validFrom
-		sig = hashlib.md5(strToSign).hexdigest()
-		params = {
-			'project': project,
-			'environment' : environment,
-			'artifact': artifactUnencoded,
-			'sig': sig,
-			'validFrom': validFrom,
-		}
-		link=BACKEND+"?"+urllib.urlencode(params)
-		
-		template_values = {
-			'params': params,
-			'link': link,
-			'version': environment+" as of "+ctime(float(validFrom))
-		}
-		self.response.headers["Content-Type"] = "text/xml"
-		path = os.path.join(os.path.dirname(__file__), 'template','buildPlist.xml')
-		self.response.out.write(template.render(path, template_values))
-    	
+        validFrom=str(int(time()))
+        artifactUnencoded=artifact.replace("|",'/').replace("%7C",'/')
+        strToSign=PRIVATEKEY+"+artifact="+artifactUnencoded+"+environment="+environment+"+project="+project+"+validFrom="+validFrom
+        sig = hashlib.md5(strToSign).hexdigest()
+        params = {
+        	'project': project,
+        	'environment' : environment,
+        	'artifact': artifactUnencoded,
+        	'sig': sig,
+        	'validFrom': validFrom,
+        }
+        link=BACKEND+"?"+urllib.urlencode(params)
+        
+        template_values = {
+        	'params': params,
+        	'link': link,
+        	'version': environment+" as of "+ctime(float(validFrom))
+        }
+        self.response.headers["Content-Type"] = "text/xml"
+        path = os.path.join(os.path.dirname(__file__), 'template','buildPlist.xml')
+        self.response.out.write(template.render(path, template_values))
+
 def main():
     application = webapp.WSGIApplication([
     	('/', MainHandler),
+        ('/addProject', addProject),
+        ('/addProjectStage', addProjectStage),
     	('/buildPlist/([^/]+)/([^/]+)/([^/]+)/a.plist', buildPlist),
     									 ],
                                          debug=True)
